0fdabffabd9f5231847b7495a45991367fdb06a3
Daniel Ferraz Alex <danielferrazalex@hotmail.com>
2023-03-08 17:44:24 -0300

    ByteBank

---
+++ ByteBank/bytebank_ATENDIMENTO/ByteBankException.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.Serialization;
+
+[Serializable]
+internal class ByteBankException : Exception
+{
+    public ByteBankException()
+    {
+    }
+
+    public ByteBankException(string? message) : base(message)
+    {
+    }
+
+    public ByteBankException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+
+    protected ByteBankException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+}
